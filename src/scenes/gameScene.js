fa05f5495e431c3785d5c285926f78565a9e1238
Theophile-Kango <fadhili.kango@gmail.com>
2020-07-09 01:09:17 +0200

    Add movement to the player

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,19 +1,44 @@
-import 'phaser';
+import Phaser from 'phaser';
 import player from '../../assets/player.png';
+
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super('Game');
   }
 
   preload() {
-    const logo = this.add.image(400, 300, 'logo').setScale(1/2);
     this.load.image('player', player);
+  }
+
+  create(){
+    this.logo = this.add.image(400, 300, 'logo').setScale(1/2);
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.speed = 3;
     this.time.delayedCall(5000, () => {
-      logo.destroy();
-      this.add.image(400, 300, 'background-loading');
-      let player = this.add.sprite(0, 560, 'player');
-      player.setScale(1/2);
-      player.setOrigin(0, 0);
+    this.logo.destroy();
+      this.add.image(400, 300, 'background-loading'); 
+      this.player1 = this.add.sprite(0, 565, 'player').setScale(1/2);
+      this.player1.setOrigin(0,0);
+      //this.player1.setBounce(0.2);
+      //this.player1.setCollideWorldBounds(true);
     }, [], this);
+    console.log(this.player1)
+ 
+  }
+ update () 
+  {
+    if(this.cursors.left.isDown){
+      this.player1.x-=this.speed;
+    }else if(this.cursors.right.isDown){
+      this.player1.x+=this.speed;
+    }
+    else{
+      if(this.cursors.down.isDown){
+        this.player1.y+=this.speed;
+      }
+      else if (this.cursors.up.isDown){
+        this.player1.y-=this.speed;
+      }
+    }
   }
 };
--- src/scenes/levels/level1.js
+++ src/scenes/levels/level1.js
@@ -1,17 +1,50 @@
-import 'phaser';
-import player from '../../../assets/logo.png';
-export default class Level1 extends Phaser.Scene {
-  constructor(){
-      super('level1');
-  }
+export default (scene) => {
+    scene.add.image(400, 300, 'background-loading');
+    //let player = scene.add.sprite(0, 560, 'player');
+    let player = scene.physics.add.sprite(0, 560, 'player');
 
-  preload() {
-    this.load.image('player', player);
-  }
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.setScale(1/2);
+    player.setOrigin(0, 0);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
-  create () {
-    this.add.image(400, 300, 'background-loading');
-    this.add.image(20, 20, 'player');
-    console.log(player);
-  }
+    //  Our player animations, turning, walking left and walking right.
+    scene.anims.create({
+        key: 'left',
+        frames: scene.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    scene.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    scene.anims.create({
+        key: 'right',
+        frames: scene.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    //  Input Events
+    //let cursors =
+     scene.input.keyboard.createCursorKeys();
+
+    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+    // stars = this.physics.add.group({
+    //     key: 'star',
+    //     repeat: 11,
+    //     setXY: { x: 12, y: 0, stepX: 70 }
+    // });
+
+    // stars.children.iterate(function (child) {
+
+    //     //  Give each star a slightly different bounce
+    //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+    // });
 }
