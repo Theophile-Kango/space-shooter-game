1e0524637f9ac1bf71d456714514760a41b89fdd
Theophile-Kango <fadhili.kango@gmail.com>
2020-07-09 04:09:33 +0200

    Add the timer and the controle of enter key

--- src/scenes/gameScene.js
+++ src/scenes/gameScene.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import player from '../../assets/player.png';
-
+import shotImg from '../../assets/shot.png';
+import shotSound from '../../assets/shot.ogg';
+import hit from '../../assets/flak_hit.ogg';
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super('Game');
@@ -8,24 +10,43 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {
     this.load.image('player', player);
+    this.load.image('shotImg', shotImg);
+    this.load.audio('shotSound', shotSound);
+    this.load.audio('hit', hit);
   }
 
   create(){
     this.logo = this.add.image(400, 300, 'logo').setScale(1/2);
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+    this.scoreText = '';
+    this.timerText = '';
+    this.levelText = '';
+    this.interval;
+    this.timer = 0;
     this.speed = 3;
+    this.level = 1;
+
     this.time.delayedCall(5000, () => {
     this.logo.destroy();
       this.add.image(400, 300, 'background-loading'); 
-      //this.player1 = this.add.sprite(0, 565, 'player').setScale(1/2);
-      this.player1 = this.physics.add.sprite(0, 600, 'player').setScale(1/2);
-      
+      this.player1 = this.physics.add.sprite(0, 600, 'player').setScale(1/2);  
       this.player1.setCollideWorldBounds(true);
+      this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
+      this.levelText = this.add.text(16, 50, `Level: ${this.level}`, { fontSize: '32px', fill: '#fff' });
+      this.timerText = this.add.text(600, 16, 'Timer: 0', { fontSize: '32px', fill: '#fff' });
+
+      this.interval = setInterval(() => {
+        this.timer += 1;
+        this.timerText.setText(`Timer: ${this.timer}`);
+      }, 1000);
+
+      this.shot = this.sound.add('shot');
+      this.hit = this.sound.add('hit');
     }, [], this);
-    console.log(this.player1)
- 
   }
- update () 
+
+  update () 
   {
     if(this.cursors.left.isDown){
       this.player1.x-=this.speed;
@@ -40,5 +61,14 @@ export default class GameScene extends Phaser.Scene {
         this.player1.y-=this.speed;
       }
     }
+
+    //this.input.on('pointerdown', this.shoot, this)
+    if(Phaser.Input.Keyboard.JustDown(this.enter)){
+      console.log('You pressed enter');
+    }
   }
+
+  // shoot(x, y){
+  //   console.log("mouse",x,y);
+  // }
 };
